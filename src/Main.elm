f8f3ee06dd267f33898d96e7bd6c625e65cbb544
David Pomerenke <david.pomerenke@uni-konstanz.de>
2021-07-10 13:31:02 +0200

    ✨ visualize raw data

--- src/Main.elm
+++ src/Main.elm
@@ -49,7 +49,7 @@ main =
 
 
 type alias Model =
-    { csv : Result String (List TimeSpanByDay)
+    { csv : Result String (List TimespanByDay)
     , hover : Bool
     }
 
@@ -63,14 +63,34 @@ init =
     )
 
 
-type alias TimeSpan a =
+type alias Timespan a =
     { from : a
     , to : a
+    , stage : Stage
     }
 
 
-type alias TimeSpanByDay =
-    { day : Int, from : Int, to : Int }
+type alias TimespanByDay =
+    { day : Int
+    , from : Int
+    , to : Int
+    , stage : Stage
+    }
+
+
+type alias UnparsedRawData =
+    { start : String, duration : String, value : String }
+
+
+type alias ParsedRawData =
+    { start : Time.Posix, duration : List Int, value : List Stage }
+
+
+type Stage
+    = Awake
+    | Light
+    | Deep
+    | Rem
 
 
 
@@ -82,7 +102,7 @@ type Msg
     | DragEnter
     | DragLeave
     | GotFiles File (List File)
-    | GotData (Result String (List TimeSpanByDay))
+    | GotData (Result String (List TimespanByDay))
     | NoOp
 
 
@@ -111,10 +131,10 @@ update msg model =
                     (Result.map
                         (Csv.Decode.decodeCsv Csv.Decode.FieldNamesFromFirstRow csvDecoder
                             >> Result.withDefault []
-                            >> List.map (\{ from, to } -> Maybe.map2 TimeSpan (parseDate from) (parseDate to))
+                            >> List.map parseRawData
                             >> Maybe.combine
                             >> Result.fromMaybe "Error parsing CSV file. Sorry."
-                            >> Result.map splitByDay
+                            >> Result.map (List.concatMap toTimespan >> splitByDay)
                         )
                         >> Result.join
                     )
@@ -144,7 +164,7 @@ readSleepData file =
         File.toBytes file
             |> Task.map
                 (Zip.fromBytes
-                    >> Maybe.map (Zip.getEntry "sleep.csv")
+                    >> Maybe.map (Zip.getEntry "raw_bed_sleep-state.csv")
                     >> Maybe.join
                     >> Maybe.map
                         (Zip.Entry.toString
@@ -157,11 +177,26 @@ readSleepData file =
         Task.succeed (Err "Please upload the Zip file.")
 
 
-csvDecoder : Csv.Decode.Decoder (TimeSpan String)
+csvDecoder : Csv.Decode.Decoder UnparsedRawData
 csvDecoder =
-    Csv.Decode.into TimeSpan
-        |> Csv.Decode.pipeline (Csv.Decode.field "from" Csv.Decode.string)
-        |> Csv.Decode.pipeline (Csv.Decode.field "to" Csv.Decode.string)
+    Csv.Decode.into UnparsedRawData
+        |> Csv.Decode.pipeline (Csv.Decode.field "start" Csv.Decode.string)
+        |> Csv.Decode.pipeline (Csv.Decode.field "duration" Csv.Decode.string)
+        |> Csv.Decode.pipeline (Csv.Decode.field "value" Csv.Decode.string)
+
+
+parseRawData : UnparsedRawData -> Maybe ParsedRawData
+parseRawData { start, duration, value } =
+    Maybe.map3 ParsedRawData
+        (parseDate start)
+        (Json.Decode.decodeString (Json.Decode.list Json.Decode.int) duration
+            |> Result.toMaybe
+        )
+        (Json.Decode.decodeString (Json.Decode.list Json.Decode.int) value
+            |> Result.toMaybe
+            |> Maybe.map (List.map toStage >> Maybe.combine)
+            |> Maybe.join
+        )
 
 
 parseDate : String -> Maybe Time.Posix
@@ -169,16 +204,67 @@ parseDate =
     Iso8601.toTime >> Result.toMaybe
 
 
-splitByDay : List (TimeSpan Time.Posix) -> List TimeSpanByDay
+toTimespan : ParsedRawData -> List (Timespan Time.Posix)
+toTimespan { start, duration, value } =
+    List.foldl
+        (\( duration_, value_ ) acc ->
+            let
+                new =
+                    { from = start
+                    , to = add (1000 * duration_) start
+                    , stage = value_
+                    }
+            in
+            case acc of
+                [] ->
+                    [ new
+                    ]
+
+                prev :: rest ->
+                    if prev.stage == value_ then
+                        { prev | to = add (1000 * duration_) prev.to } :: rest
+
+                    else
+                        new :: prev :: rest
+        )
+        []
+        (List.zip duration value)
+
+
+toStage : Int -> Maybe Stage
+toStage i =
+    case i of
+        0 ->
+            Just Awake
+
+        1 ->
+            Just Light
+
+        2 ->
+            Just Deep
+
+        3 ->
+            Just Rem
+
+        _ ->
+            Nothing
+
+
+add : Int -> Time.Posix -> Time.Posix
+add millis_ time_ =
+    Time.millisToPosix (millis_ + Time.posixToMillis time_)
+
+
+splitByDay : List (Timespan Time.Posix) -> List TimespanByDay
 splitByDay =
     List.concatMap
-        (\{ from, to } ->
+        (\{ from, to, stage } ->
             if toDay from == toDay to then
-                [ TimeSpanByDay (toDay from) (toTimeOfDay from) (toTimeOfDay to) ]
+                [ TimespanByDay (toDay from) (toTimeOfDay from) (toTimeOfDay to) stage ]
 
             else
-                [ TimeSpanByDay (toDay from) (toTimeOfDay from) millisPerDay
-                , TimeSpanByDay (toDay to) 0 (toTimeOfDay to)
+                [ TimespanByDay (toDay from) (toTimeOfDay from) millisPerDay stage
+                , TimespanByDay (toDay to) 0 (toTimeOfDay to) stage
                 ]
         )
 
@@ -277,8 +363,14 @@ vis content =
                 widgetWidth
                 widgetHeight
                 (List.map
-                    (\{ day, from, to } ->
-                        bar ( toX from, toFloat (day - minDay) * (barHeight + barDist) ) (toX (to - from)) widgetHeight
+                    (\{ day, from, to, stage } ->
+                        bar
+                            ( toX from
+                            , toFloat (day - minDay) * (barHeight + barDist)
+                            )
+                            stage
+                            (toX (to - from))
+                            widgetHeight
                     )
                     content
                 )
@@ -298,10 +390,26 @@ barDist =
     5
 
 
-bar ( x, y ) barWidth widgetHeight =
+bar ( x, y ) stage barWidth widgetHeight =
     rect barWidth barHeight
-        |> filled GraphicSVG.blue
+        |> filled (toColor stage)
         |> move
             ( -widgetWidth / 2 + x + barWidth / 2
             , -widgetHeight / 2 + y + barHeight / 2
             )
+
+
+toColor : Stage -> GraphicSVG.Color
+toColor stage =
+    case stage of
+        Awake ->
+            yellow
+
+        Light ->
+            lightBlue
+
+        Deep ->
+            blue
+
+        Rem ->
+            purple
